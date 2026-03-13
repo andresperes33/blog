aa9d141654187f48a6b66d35fd7e3048c2053d93
andresperes33 <andresperesdev@gmail.com>
2026-03-13 14:42:47 -0300

    fix: importa ContactView em urls.py para resolver NameError na VPS

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ReviewListView, ReviewDetailView, CategoryListView, CategoryDetailView, AboutView, AllReviewsView, PrivacyView, TermsView
+from .views import ReviewListView, ReviewDetailView, CategoryListView, CategoryDetailView, AboutView, AllReviewsView, PrivacyView, TermsView, ContactView
 
 app_name = 'reviews'
 
